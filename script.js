81d27b349852ea65956cf36188211de83e0ad334
Vadim <vadim.prog.2000@gmail.com>
2021-02-01 00:51:36 +0300

    complete fractal trees random

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
 
-let curve;
+let curve = 10;
+let curve2 = 0;
 
 function drawTree(startX, startY, len, angle, branchWidth, colorTree, colorLeaf) {
   ctx.beginPath();
@@ -15,16 +16,16 @@ function drawTree(startX, startY, len, angle, branchWidth, colorTree, colorLeaf)
   ctx.strokeStyle = colorTree;
   ctx.fillStyle = colorLeaf;
   ctx.shadowBlur = 5;
-  ctx.shadowColor = colorLeaf;
+  ctx.shadowColor = 'rgba(255,255,255, .5)';
   ctx.lineWidth = branchWidth;
   ctx.translate(startX, startY);
   ctx.rotate(angle * Math.PI/180);
   ctx.moveTo(0, 0);
   // ctx.lineTo(0, -len);
   if (angle > 0) {
-    ctx.bezierCurveTo(10, -len/2, 30, -len/2, 0, -len);
+    ctx.bezierCurveTo(curve2, -len/2, curve2, -len/2, 0, -len);
   } else {
-    ctx.bezierCurveTo(10, -len/2, -30, -len/2, 0, -len);
+    ctx.bezierCurveTo(curve2, -len/2, curve2, -len/2, 0, -len);
   }
   ctx.stroke();
 
@@ -36,15 +37,11 @@ function drawTree(startX, startY, len, angle, branchWidth, colorTree, colorLeaf)
     return;
   }
 
-  curve = Math.random() * 10 + 10;
-
   drawTree(0, -len, len * 0.8, angle + curve, branchWidth * 0.6);
   drawTree(0, -len, len * 0.8, angle - curve, branchWidth * 0.6);
 
   ctx.restore();
 }
-
-// drawTree(canvas.width/2, canvas.height - 80, 120, 0, 10, 'rgb(96, 30, 161)', 'rgb(46, 238, 228)');
 
 function generateRandomTree() {
   ctx.clearRect(0,0,canvas.width, canvas.height);
@@ -52,10 +49,15 @@ function generateRandomTree() {
   const centerPointX = canvas.width /2;
   const len = Math.floor((Math.random() * 60) + 60);
   const angle = 0;
-  const branchWidth = (Math.random() * 99) + 1;
+  const branchWidth = (Math.random() * 30) + 10;
   const colorTree = 'rgb(' + Math.random() * 255 + ',' + Math.random() * 255 + ',' + Math.random() * 255 + ')';
   const colorLeaf = 'rgb(' + Math.random() * 255 + ',' + Math.random() * 255 + ',' + Math.random() * 255 + ')';
-  console.log(colorTree)
+  curve = Math.random() * 20 + 1;
+  curve2 = Math.random() * 50
+
+  generateButton.style.backgroundColor = colorTree
+  generateButton.style.color = colorLeaf
+
   drawTree(centerPointX, canvas.height - 80, len, angle, branchWidth, colorTree, colorLeaf)
 }
 
