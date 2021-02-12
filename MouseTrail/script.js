f7c11f88ffb8ee681071702fc7127cb13837000e
Vadim <vadim.prog.2000@gmail.com>
2021-02-12 23:58:56 +0300

    fix rubber for canvas rect

--- MouseTrail/script.js
+++ MouseTrail/script.js
@@ -1,5 +1,18 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.fillStyle = 'white';
+  ctx.fillRect(10, 10, 150, 50)
+  ctx.fillStyle = 'red';
+  ctx.fillRect(100, 100, 500, 150)
+})
 
 ctx.fillStyle = 'white';
 ctx.fillRect(10, 10, 150, 50)
+ctx.fillStyle = 'red';
+ctx.fillRect(100, 100, 500, 150)
