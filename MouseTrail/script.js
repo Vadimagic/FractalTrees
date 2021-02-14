6ff49feed9923a5bdd1b6ca82868480d3971cf8f
Vadim <vadim.prog.2000@gmail.com>
2021-02-14 23:47:54 +0300

    Mouse trail/ add amazing move particles when mouse move

--- MouseTrail/script.js
+++ MouseTrail/script.js
@@ -22,53 +22,54 @@ canvas.addEventListener('click', (event) => {
 canvas.addEventListener('mousemove', (event) => {
   mouse.x = event.x;
   mouse.y = event.y;
+  for (let i = 0; i < 5; i++) {
+    particlesArray.push(new Particle());
+  }
 })
-
-// ctx.strokeStyle = 'red';
-// ctx.lineWidth = 4;
-// ctx.stroke();
 
 class Particle {
   constructor() {
-    // this.x = mouse.x;
-    // this.y = mouse.y;
-    this.x = Math.random() * canvas.width
-    this.y = Math.random() * canvas.height
-    this.size = Math.random() * 5 + 1;
+    this.x = mouse.x;
+    this.y = mouse.y;
+    // this.x = Math.random() * canvas.width
+    // this.y = Math.random() * canvas.height
+    this.size = Math.random() * 15 + 1;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
   }
   update() {
     this.x += this.speedX;
     this.y += this.speedY;
+    if (this.size > 0.2) {
+      this.size -= 0.1;
+    }
   }
   draw() {
-    ctx.fillStyle = '#813';
-    ctx.strokeStyle = 'red';
-    ctx.lineWidth = 4;
+    ctx.fillStyle = 'white';
+    // ctx.strokeStyle = '#81f';
+    // ctx.lineWidth = 1;
     ctx.beginPath();
-    ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fill();
-    ctx.stroke();
+    // ctx.stroke();
   }
 }
-
-function init() {
-  for (let i = 0; i < 100; i++) {
-    particlesArray.push(new Particle());
-  }
-}
-init();
 
 function handleParticles() {
   for (let i = 0; i < particlesArray.length; i++) {
     particlesArray[i].update();
     particlesArray[i].draw();
+    if (particlesArray[i].size <= 0.2) {
+      particlesArray.splice(i, 1);
+      i--;
+    }
   }
 }
 
 function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'rgba(0,0,0,.01)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
   handleParticles();
   requestAnimationFrame(animate);
 }
