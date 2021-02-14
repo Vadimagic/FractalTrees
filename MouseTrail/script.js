20fad53e76c23cc5da47cae2bbfd2140eaa4b614
Vadim <vadim.prog.2000@gmail.com>
2021-02-14 23:28:31 +0300

    Mouse trail/ create init particles in array & handle particles

--- MouseTrail/script.js
+++ MouseTrail/script.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const particlesArray = [];
 
 window.addEventListener('resize', () => {
   canvas.width = window.innerWidth;
@@ -29,8 +30,10 @@ canvas.addEventListener('mousemove', (event) => {
 
 class Particle {
   constructor() {
-    this.x = mouse.x;
-    this.y = mouse.y;
+    // this.x = mouse.x;
+    // this.y = mouse.y;
+    this.x = Math.random() * canvas.width
+    this.y = Math.random() * canvas.height
     this.size = Math.random() * 5 + 1;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
@@ -50,8 +53,23 @@ class Particle {
   }
 }
 
+function init() {
+  for (let i = 0; i < 100; i++) {
+    particlesArray.push(new Particle());
+  }
+}
+init();
+
+function handleParticles() {
+  for (let i = 0; i < particlesArray.length; i++) {
+    particlesArray[i].update();
+    particlesArray[i].draw();
+  }
+}
+
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleParticles();
   requestAnimationFrame(animate);
 }
 animate();
