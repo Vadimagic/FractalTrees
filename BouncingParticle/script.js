5c959d4a2093726be0f9f84b5fb75c196f03d0ba
Vadim <vadim.prog.2000@gmail.com>
2021-02-27 23:59:35 +0300

    Bouncing Particle/ fix name particleArrays to particleArray

--- BouncingParticle/script.js
+++ BouncingParticle/script.js
@@ -83,16 +83,16 @@ function connect() {
   let opacityValue = 1;
   for (let a = 0; a < particleArray.length; a++) {
     for (let b = 0; b < particleArray.length; b++) {
-      const dx = particlesArray[a].x - particlesArray[b].x;
-      const dy = particlesArray[a].y - particlesArray[b].y;
+      const dx = particleArray[a].x - particleArray[b].x;
+      const dy = particleArray[a].y - particleArray[b].y;
       const distance = Math.sqrt(dx * dx + dy * dy);
       if (distance < 200) {
         opacityValue = 1 - (distance / 10000);
         ctx.fillStyle('rgba(0,0,0,' + opacityValue + ')');
         ctx.beginPath();
         ctx.lineWidth = 1;
-        ctx.moveTo(particlesArray[a].x, particlesArray[a].y);
-        ctx.lineTo(particlesArray[b].x, particlesArray[b].y);
+        ctx.moveTo(particleArray[a].x, particleArray[a].y);
+        ctx.lineTo(particleArray[b].x, particleArray[b].y);
         ctx.stroke();
         ctx.closePath();
       }
